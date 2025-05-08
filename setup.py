8353992631ee38a1d4cb2ab7b39b9e03f09bd071
Colin Sears <colin.sears17@gmail.com>
2025-05-08 13:51:48 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,26 @@ from setuptools import setup, find_packages
 
 setup(
     name="pipedream",
-    version="0.1.0",
+    version="1.0.0",
     description="Pipedream Satellite GUI and Controller",
     author="Colin Sears",
     author_email="colin.sears17@gmail.com",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        "opencv-python",
-        "Pillow",
-        "evdev",         
-        "pyserial",
+        "colorzero==2.0",
+        "evdev==1.9.1",
+        "gpiozero==2.0.1",
+        "imutils==0.5.4",
+        "numpy==2.2.4",
+        "opencv-python==4.11.0.86",
+        "pillow==11.2.1",
+        "pyserial==3.5",
+        "RPi.GPIO==0.7.1",
     ],
     entry_points={
         "console_scripts": [
-            "pipedream=cli:main",
+            "pipedream=pipedream.cli:main",
         ],
     },
     classifiers=[
